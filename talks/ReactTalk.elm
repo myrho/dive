738408f19cdf0ff1f32be1f647f24aba7a2959eb
Matthias <git@myrho.net>
2016-11-28 09:40:55 +0100

    more dummy presentation data

--- talks/ReactTalk.elm
+++ talks/ReactTalk.elm
@@ -1,20 +1,41 @@
-module ReactTalk exposing (..)
+module DummyTalk exposing (..)
 
 import Color exposing (Color, black)
-import Window exposing (Size)
-import Mouse exposing (Position)
 import Dive.Model exposing (..)
+import ElmLogo
 
 world =
-  [ Text 
-    <| TextObject "Hello World!" black "Helvetica" 12 
-    <| Position 0 0
+  ElmLogo.logo
+  ++
+  [ Text
+    <| TextObject "How to build this presentation in Elm" Color.white "Helvetica" 1
+    <| Position 500 250
+  , Text
+    <| TextObject "type alias Model =" Color.white "Courier New" 1
+    <| Position 500 249
+  , Text
+    <| TextObject "  { bla : String" Color.white "Courier New" 1
+    <| Position 500 248
+  , Text
+    <| TextObject "  , x : Int" Color.white "Courier New" 1
+    <| Position 500 247
+  , Text
+    <| TextObject "  }" Color.white "Courier New" 1
+    <| Position 500 246
   ]
 
 keys = 
   Keys
     []
-    (Key (Position 0 0) (Size 400 300))
-    [ Key (Position 0 0) (Size 200 100)
-    , Key (Position 0 0) (Size 800 600)
+    (Key (Position 500 500) (Size 4196 3072))
+    [ Key (Position 490 250) (Size 10.2 7.6)
+    , Key (Position 503 249) (Size 10.2 7.6)
+    , Key (Position 503 248) (Size 10.2 7.6)
     ]
+
+moveTo (cx, cy) list =
+  List.map 
+    (\(x,y) ->
+      (x + cx, y + cy)
+    )
+    list
