f3bda59715268028d0524f10a3b33250351d4d6d
Matthias <git@myrho.net>
2016-11-30 10:19:33 +0100

    fix dummytalk

--- talks/DummyTalk.elm
+++ talks/DummyTalk.elm
@@ -9,19 +9,19 @@ world =
   (Transform.transformWorld (Position -500 -500) (Size 1000 1000) ElmLogo.logo)
   ++
   [ Text
-    <| TextObject "How to build this presentation in Elm" Color.white "Helvetica" 0.1
+    <| TextObject "How to build this presentation in Elm" Color.white "Helvetica" 0.1 Center 1
     <| Position 500 250
   , Text
-    <| TextObject "type alias Model =" Color.white "Courier New" 1
+    <| TextObject "type alias Model =" Color.white "Courier New" 1 Center 1
     <| Position 500 249
   , Text
-    <| TextObject "  { bla : String" Color.white "Courier New" 1
+    <| TextObject "  { bla : String" Color.white "Courier New" 1 Center 1
     <| Position 500 248
   , Text
-    <| TextObject "  , x : Int" Color.white "Courier New" 1
+    <| TextObject "  , x : Int" Color.white "Courier New" 1 Center 1
     <| Position 500 247
   , Text
-    <| TextObject "  }" Color.white "Courier New" 1
+    <| TextObject "  }" Color.white "Courier New" 1 Center 1
     <| Position 500 246
   , Image
     <| ImageObject 
@@ -34,13 +34,13 @@ world =
 frames = 
   Frames
     []
-    (Frame (Position 500 500) (Size 4196 3072) 0)
     [ Frame (Position 490 250) (Size 10.2 7.6) 1000
     , Frame (Position 499 250) (Size 1.02 0.76) 500
     , Frame (Position 503 248) (Size 10.2 7.6) 20
     , Frame (Position 500 500) (Size 2048 1500) 0
     , Frame (Position 300 1200) (Size 200 200) 2000
     ]
+    (Frame (Position 500 500) (Size 4196 3072) 0)
 
 moveTo (cx, cy) list =
   List.map 
