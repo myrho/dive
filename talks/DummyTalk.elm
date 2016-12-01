dd1155fcf206ef0652d0f9762be691b6a1b0e371
Matthias <git@myrho.net>
2016-12-01 11:58:36 +0100

    update Dummytalk

--- talks/DummyTalk.elm
+++ talks/DummyTalk.elm
@@ -6,10 +6,10 @@ import Dive.Transform as Transform
 import ElmLogo
 
 world =
-  (Transform.transformWorld (Position -500 -500) (Size 1000 1000) ElmLogo.logo)
+  (Transform.transformWorld (Position 0 0) (Size 1000 1000) ElmLogo.logo)
   ++
   [ Text
-    <| TextObject "How to build this presentation in Elm" Color.white "Helvetica" 0.1 Center 1
+    <| TextObject "Hello World" Color.white "Helvetica" 0.1 Center 1
     <| Position 500 250
   , Text
     <| TextObject "type alias Model =" Color.white "Courier New" 1 Center 1
@@ -25,7 +25,7 @@ world =
     <| Position 500 246
   , Image
     <| ImageObject 
-        "/talks/smiley.png"
+        "talks/smiley.png"
         200
         200
     <| Position 300 1200
@@ -40,7 +40,7 @@ frames =
     , Frame (Position 500 500) (Size 2048 1500) 0
     , Frame (Position 300 1200) (Size 200 200) 2000
     ]
-    (Frame (Position 500 500) (Size 4196 3072) 0)
+    (Frame (Position 0 0) (Size 4196 3072) 0)
 
 moveTo (cx, cy) list =
   List.map 
